c06a234a43a9335901cbd11f92193246af6b5fdb
Shuntaro Aoki <aoki@atr.jp>
2017-10-17 15:16:41 +0900

    python: move main() on the top of scripts

--- code/python/analysis_FeaturePrediction.py
+++ code/python/analysis_FeaturePrediction.py
@@ -37,110 +37,6 @@ from bdpy.distcomp import DistComp
 import god_config as config
 
 
-# Functions ############################################################
-
-def feature_prediction(x_train, y_train, x_test, y_test, n_voxel=500, n_iter=200):
-    '''Run feature prediction
-
-    Parameters
-    ----------
-    x_train, y_train : array_like [shape = (n_sample, n_voxel)]
-        Brain data and image features for training
-    x_test, y_test : array_like [shape = (n_sample, n_unit)]
-        Brain data and image features for test
-    n_voxel : int
-        The number of voxels
-    n_iter : int
-        The number of iterations
-
-    Returns
-    -------
-    predicted_label : array_like [shape = (n_sample, n_unit)]
-        Predicted features
-    ture_label : array_like [shape = (n_sample, n_unit)]
-        True features in test data
-    '''
-
-    n_unit = y_train.shape[1]
-
-    # Normalize brian data (x)
-    norm_mean_x = np.mean(x_train, axis=0)
-    norm_scale_x = np.std(x_train, axis=0, ddof=1)
-
-    x_train = (x_train - norm_mean_x) / norm_scale_x
-    x_test = (x_test - norm_mean_x) / norm_scale_x
-
-    # Feature prediction for each unit
-    print('Running feature prediction')
-
-    y_true_list = []
-    y_pred_list = []
-
-    for i in range(n_unit):
-
-        print('Unit %03d' % (i + 1))
-        start_time = time()
-
-        # Get unit features
-        y_train_unit = y_train[:, i]
-        y_test_unit =  y_test[:, i]
-
-        # Normalize image features for training (y_train_unit)
-        norm_mean_y = np.mean(y_train_unit, axis=0)
-        std_y = np.std(y_train_unit, axis=0, ddof=1)
-        norm_scale_y = 1 if std_y == 0 else std_y
-        
-        y_train_unit = (y_train_unit - norm_mean_y) / norm_scale_y
-
-        # Voxel selection
-        corr = corrcoef(y_train_unit, x_train, var='col')
-
-        x_train_unit, voxel_index = select_top(x_train, np.abs(corr), n_voxel, axis=1, verbose=False)
-        x_test_unit = x_test[:, voxel_index]
-
-        # Add bias terms
-        x_train_unit = add_bias(x_train_unit, axis=1)
-        x_test_unit = add_bias(x_test_unit, axis=1)
-
-        # Setup regression
-        # For quick demo, use linaer regression
-        model = LinearRegression()
-        #model = SparseLinearRegression(n_iter=n_iter, prune_mode=1)
-
-        # Training and test
-        try:
-            model.fit(x_train_unit, y_train_unit)  # Training
-            y_pred = model.predict(x_test_unit)    # Test
-        except:
-            # When SLiR failed, returns zero-filled array as predicted features
-            y_pred = np.zeros(y_test_unit.shape)
-
-        # Denormalize predicted features
-        y_pred = y_pred * norm_scale_y + norm_mean_y
-
-        y_true_list.append(y_test_unit)
-        y_pred_list.append(y_pred)
-
-        print('Time: %.3f sec' % (time() - start_time))
-
-    # Create numpy arrays for return values
-    y_predicted = np.vstack(y_pred_list).T
-    y_true = np.vstack(y_true_list).T
-
-    return y_predicted, y_true
-
-
-def get_averaged_feature(pred_y, true_y, labels):
-    '''Return category-averaged features'''
-
-    labels_set = np.unique(labels)
-
-    pred_y_av = np.array([np.mean(pred_y[labels == c, :], axis=0) for c in labels_set])
-    true_y_av = np.array([np.mean(true_y[labels == c, :], axis=0) for c in labels_set])
-
-    return pred_y_av, true_y_av, labels_set
-
-
 # Main #################################################################
 
 def main():
@@ -297,6 +193,112 @@ def main():
 
         dist.unlock()
 
+
+# Functions ############################################################
+
+def feature_prediction(x_train, y_train, x_test, y_test, n_voxel=500, n_iter=200):
+    '''Run feature prediction
+
+    Parameters
+    ----------
+    x_train, y_train : array_like [shape = (n_sample, n_voxel)]
+        Brain data and image features for training
+    x_test, y_test : array_like [shape = (n_sample, n_unit)]
+        Brain data and image features for test
+    n_voxel : int
+        The number of voxels
+    n_iter : int
+        The number of iterations
+
+    Returns
+    -------
+    predicted_label : array_like [shape = (n_sample, n_unit)]
+        Predicted features
+    ture_label : array_like [shape = (n_sample, n_unit)]
+        True features in test data
+    '''
+
+    n_unit = y_train.shape[1]
+
+    # Normalize brian data (x)
+    norm_mean_x = np.mean(x_train, axis=0)
+    norm_scale_x = np.std(x_train, axis=0, ddof=1)
+
+    x_train = (x_train - norm_mean_x) / norm_scale_x
+    x_test = (x_test - norm_mean_x) / norm_scale_x
+
+    # Feature prediction for each unit
+    print('Running feature prediction')
+
+    y_true_list = []
+    y_pred_list = []
+
+    for i in range(n_unit):
+
+        print('Unit %03d' % (i + 1))
+        start_time = time()
+
+        # Get unit features
+        y_train_unit = y_train[:, i]
+        y_test_unit =  y_test[:, i]
+
+        # Normalize image features for training (y_train_unit)
+        norm_mean_y = np.mean(y_train_unit, axis=0)
+        std_y = np.std(y_train_unit, axis=0, ddof=1)
+        norm_scale_y = 1 if std_y == 0 else std_y
+
+        y_train_unit = (y_train_unit - norm_mean_y) / norm_scale_y
+
+        # Voxel selection
+        corr = corrcoef(y_train_unit, x_train, var='col')
+
+        x_train_unit, voxel_index = select_top(x_train, np.abs(corr), n_voxel, axis=1, verbose=False)
+        x_test_unit = x_test[:, voxel_index]
+
+        # Add bias terms
+        x_train_unit = add_bias(x_train_unit, axis=1)
+        x_test_unit = add_bias(x_test_unit, axis=1)
+
+        # Setup regression
+        # For quick demo, use linaer regression
+        model = LinearRegression()
+        #model = SparseLinearRegression(n_iter=n_iter, prune_mode=1)
+
+        # Training and test
+        try:
+            model.fit(x_train_unit, y_train_unit)  # Training
+            y_pred = model.predict(x_test_unit)    # Test
+        except:
+            # When SLiR failed, returns zero-filled array as predicted features
+            y_pred = np.zeros(y_test_unit.shape)
+
+        # Denormalize predicted features
+        y_pred = y_pred * norm_scale_y + norm_mean_y
+
+        y_true_list.append(y_test_unit)
+        y_pred_list.append(y_pred)
+
+        print('Time: %.3f sec' % (time() - start_time))
+
+    # Create numpy arrays for return values
+    y_predicted = np.vstack(y_pred_list).T
+    y_true = np.vstack(y_true_list).T
+
+    return y_predicted, y_true
+
+
+def get_averaged_feature(pred_y, true_y, labels):
+    '''Return category-averaged features'''
+
+    labels_set = np.unique(labels)
+
+    pred_y_av = np.array([np.mean(pred_y[labels == c, :], axis=0) for c in labels_set])
+    true_y_av = np.array([np.mean(true_y[labels == c, :], axis=0) for c in labels_set])
+
+    return pred_y_av, true_y_av, labels_set
+
+
+# Run as a scirpt ######################################################
 
 if __name__ == '__main__':
     # To avoid any use of global variables,
